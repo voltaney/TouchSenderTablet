c8788ea37c8ecf0966c5e121c5a4bc5cf82aeef0
voltaney <voltaney61@gmail.com>
2025-03-29 17:19:43 +0900

    SettingsCardによるテーマ変更実装

--- TouchSenderTablet.GUI/Views/SettingsPage.xaml.cs
+++ TouchSenderTablet.GUI/Views/SettingsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.UI.Xaml.Controls;
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
+using TouchSenderTablet.GUI.Helpers;
 using TouchSenderTablet.GUI.ViewModels;
 
 namespace TouchSenderTablet.GUI.Views;
@@ -16,5 +18,14 @@ public sealed partial class SettingsPage : Page
     {
         ViewModel = App.GetService<SettingsViewModel>();
         InitializeComponent();
+    }
+
+    private void OnThemeSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var selectedTheme = ((ComboBoxItem)ThemeMode.SelectedItem)?.Tag?.ToString();
+        if (selectedTheme != null)
+        {
+            ViewModel.SwitchThemeCommand.Execute(EnumHelper.GetEnum<ElementTheme>(selectedTheme));
+        }
     }
 }
