13b62e0264e06ebb15eb8c0b5641b30f05bc5fd5
voltaney <voltaney61@gmail.com>
2025-03-30 14:13:27 +0900

    P/Invoke メソッドの宣言を LibraryImport に変更
    `GetActiveWindow` と `SendMessage` メソッドの宣言を
    `DllImport` 属性から `LibraryImport` 属性に変更しました。
    これにより、C# 10 以降の新しい P/Invoke 機能を活用し、
    `GetActiveWindow` メソッドには `partial` 修飾子を追加し、
    `SendMessage` メソッドには `EntryPoint` 属性を指定して
    `SendMessageW` を明示的に呼び出すようにしました。

--- TouchSenderTablet.GUI/Helpers/TitleBarHelper.cs
+++ TouchSenderTablet.GUI/Helpers/TitleBarHelper.cs
@@ -12,17 +12,17 @@ namespace TouchSenderTablet.GUI.Helpers;
 // Helper class to workaround custom title bar bugs.
 // DISCLAIMER: The resource key names and color values used below are subject to change. Do not depend on them.
 // https://github.com/microsoft/TemplateStudio/issues/4516
-internal class TitleBarHelper
+internal partial class TitleBarHelper
 {
     private const int WAINACTIVE = 0x00;
     private const int WAACTIVE = 0x01;
     private const int WMACTIVATE = 0x0006;
 
-    [DllImport("user32.dll")]
-    private static extern IntPtr GetActiveWindow();
+    [LibraryImport("user32.dll")]
+    private static partial IntPtr GetActiveWindow();
 
-    [DllImport("user32.dll", CharSet = CharSet.Auto)]
-    private static extern IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, IntPtr lParam);
+    [LibraryImport("user32.dll", EntryPoint = "SendMessageW")]
+    private static partial IntPtr SendMessage(IntPtr hWnd, int msg, int wParam, IntPtr lParam);
 
     public static void UpdateTitleBar(ElementTheme theme)
     {
